03dd9a1148a4f16cd307fc2e6f68ad9258ea86f7
Harman <harmanpreet181001@gmail.com>
2022-03-04 16:58:01 +0530

    Added: todo api

--- routes/todoApi.js
+++ routes/todoApi.js
@@ -7,6 +7,8 @@ const path = require('path')
 const route = express.Router()
 const db = new Database(path.resolve("./data.db"), { verbose: console.log, fileMustExist: true })
 
+route.use(express.json())
+
 route.use(function(req, res, next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
@@ -22,6 +24,25 @@ route.use(function(req, res, next){
         req.user = user
         next()
     })
+})
+
+route.get('/', (req, res) => {
+    const todos = db.prepare('SELECT * FROM todos').all()
+    res.status(200).json({ data: todos })
+})
+
+route.post('/create', (req, res) => {
+    const stmt = db.prepare('INSERT INTO todos("task", "user_id") VALUES (?, ?)').run(req.body.task, req.user.id)
+    if(stmt.changes == 0) return res.status(400).json({ msg: "Error" })
+    res.status(201).json({ msg: 'success' })
+})
+
+route.delete('/delete', (req, res) => {
+    const check = db.prepare('SELECT * FROM todos WHERE id = ?').get(req.body.task_id)
+    if(!check) return res.status(404).json({ msg:"no such task exists" })
+    const stmt = db.prepare('DELETE FROM todos WHERE id = ?').run(req.body.task_id)
+    if(stmt.changes == 0) return res.status(400).json({ msg: "Error" })
+    res.status(200).json({ msg: 'success' })
 })
 
 module.exports = route
--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const authApi = require('./routes/authApi')
 const todoApi = require('./routes/todoApi')
+const helmet = require('helmet')
+
+app.use(helmet())
 
 app.use('/auth', authApi)
 
